374a3cfc15e988bc0e3acaa311f76148799c2821
Jason-Wanxt <wanxutao123@gmail.com>
2025-11-26 01:53:23 +0800

    add question db integration

--- src/notion.ts
+++ src/notion.ts
@@ -3,164 +3,188 @@ import {
   isFullPage,
   isFullBlock,
   collectPaginatedAPI,
-} from '@notionhq/client'
+} from '@notionhq/client';
 import {
   PageObjectResponse,
   BlockObjectResponse,
   QueryDatabaseParameters,
   PartialPageObjectResponse,
   ListBlockChildrenResponse,
-  GetPageResponse
-} from '@notionhq/client/build/src/api-endpoints'
+  GetPageResponse,
+} from '@notionhq/client/build/src/api-endpoints';
 
 export interface Page {
-  page: PageObjectResponse
-  blocks: Block[]
+  page: PageObjectResponse;
+  blocks: Block[];
 }
 
 export interface Block {
-  block: BlockObjectResponse
-  children: Block[]
+  block: BlockObjectResponse;
+  children: Block[];
 }
 
-export type IconItemResponse = {
-  type: "emoji"
-  emoji: string   // EmojiRequest
-} | {
-  type: "external"
-  external: {
-      url: string // TextRequest
-  }
-} | {
-  type: "file"
-  file: {
-      url: string
-      expiry_time: string
-  }
-} | null
+export type IconItemResponse =
+  | {
+      type: 'emoji';
+      emoji: string; // EmojiRequest
+    }
+  | {
+      type: 'external';
+      external: {
+        url: string; // TextRequest
+      };
+    }
+  | {
+      type: 'file';
+      file: {
+        url: string;
+        expiry_time: string;
+      };
+    }
+  | null;
 
 export const API_REQUEST_DEFAULT_OPTIONS = {
   attempts: 3,
-  delaySeconds: 5
-}
+  delaySeconds: 5,
+};
 export interface APIRequestOptions {
-    attempts: number,
-    delaySeconds: number
+  attempts: number;
+  delaySeconds: number;
 }
 
 const wait = (delay: number) =>
-  new Promise(resolve => setTimeout(resolve, delay))
+  new Promise((resolve) => setTimeout(resolve, delay));
 
 export async function queryDatabase(
   client: Client,
   params: QueryDatabaseParameters,
-  options:APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS,
+  options: APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS
 ): Promise<PageObjectResponse[]> {
-  let pages: (PageObjectResponse | PartialPageObjectResponse)[]
+  let pages: (PageObjectResponse | PartialPageObjectResponse)[];
   try {
-    pages = await collectPaginatedAPI(client.databases.query, params)
-  } catch(err:any){
-    if (err.status == 502 || err.status == 500){
+    pages = await collectPaginatedAPI(client.databases.query, params);
+  } catch (err: any) {
+    if (err.status == 502 || err.status == 500) {
       const attemptsLeft = options.attempts - 1;
-      if (attemptsLeft > 0){
-        console.log(`DQ query ${err.status} error: ${attemptsLeft} attempts remaining.`);
-        console.log(` Waiting ${options.delaySeconds} seconds and trying again`);
+      if (attemptsLeft > 0) {
+        console.log(
+          `DQ query ${err.status} error: ${attemptsLeft} attempts remaining.`
+        );
+        console.log(
+          ` Waiting ${options.delaySeconds} seconds and trying again`
+        );
         await wait(options.delaySeconds * 1000);
-        return queryDatabase(client, params, {...options, attempts: attemptsLeft})
+        return queryDatabase(client, params, {
+          ...options,
+          attempts: attemptsLeft,
+        });
       }
     }
-    throw err
+    throw err;
   }
-  const fullPages: PageObjectResponse[] = []
+  const fullPages: PageObjectResponse[] = [];
   for (const page of pages) {
     if (!isFullPage(page)) {
-      throw new Error('Found non-full page')
+      throw new Error('Found non-full page');
     }
-    fullPages.push(page)
+    fullPages.push(page);
   }
-  return fullPages
+  return fullPages;
 }
 
 export async function queryDatabaseWithBlocks(
   client: Client,
   params: QueryDatabaseParameters,
-  options:APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS,
+  options: APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS
 ): Promise<Page[]> {
-  const fullPages = await queryDatabase(client, params, options)
+  const fullPages = await queryDatabase(client, params, options);
   const children = await Promise.all(
-    fullPages.map(page => {
-      return getBlockChildren(client, page.id, options)
+    fullPages.map((page) => {
+      return getBlockChildren(client, page.id, options);
     })
-  )
+  );
   return fullPages.map((page, i) => {
     return {
       page: page,
       blocks: children[i],
-    }
-  })
+    };
+  });
 }
 
 export async function getPageWithBlocks(
   client: Client,
   pageId: string,
-  options: APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS,
+  options: APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS
 ): Promise<Page> {
-  let page: GetPageResponse;  
+  let page: GetPageResponse;
   try {
-    page = await client.pages.retrieve({page_id: pageId})
-  } catch(err:any){
-    if (err.status == 502 || err.status == 500){
+    page = await client.pages.retrieve({ page_id: pageId });
+  } catch (err: any) {
+    if (err.status == 502 || err.status == 500) {
       const attemptsLeft = options.attempts - 1;
-      if (attemptsLeft > 0){
-        console.log(`Get pages ${err.status} error: ${attemptsLeft} attempts remaining.`);
-        console.log(` Waiting ${options.delaySeconds} seconds and trying again`);
+      if (attemptsLeft > 0) {
+        console.log(
+          `Get pages ${err.status} error: ${attemptsLeft} attempts remaining.`
+        );
+        console.log(
+          ` Waiting ${options.delaySeconds} seconds and trying again`
+        );
         await wait(options.delaySeconds * 1000);
-        return getPageWithBlocks(client, pageId, {...options, attempts: attemptsLeft})
+        return getPageWithBlocks(client, pageId, {
+          ...options,
+          attempts: attemptsLeft,
+        });
       }
     }
-    throw err
-
+    throw err;
   }
   if (!isFullPage(page)) {
-      throw new Error('Found non-full page')
-    }
+    throw new Error('Found non-full page');
+  }
   return {
     page: page,
     blocks: await getBlockChildren(client, page.id, options),
-  }
+  };
 }
 
 async function getBlockChildren(
   client: Client,
   block_id: string,
-  options: APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS,
+  options: APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS
 ): Promise<Block[]> {
   let blocks: ListBlockChildrenResponse;
   try {
-    blocks = await client.blocks.children.list({ block_id })
-  } catch(err:any){
-    if (err.status == 502 || err.status == 500){
+    blocks = await client.blocks.children.list({ block_id });
+  } catch (err: any) {
+    if (err.status == 502 || err.status == 500) {
       const attemptsLeft = options.attempts - 1;
-      if (attemptsLeft > 0){
-        console.log(`Get Block Children ${err.status} error: ${attemptsLeft} attempts remaining.`);
-        console.log(` Waiting ${options.delaySeconds} seconds and trying again`);
+      if (attemptsLeft > 0) {
+        console.log(
+          `Get Block Children ${err.status} error: ${attemptsLeft} attempts remaining.`
+        );
+        console.log(
+          ` Waiting ${options.delaySeconds} seconds and trying again`
+        );
         await wait(options.delaySeconds * 1000);
-        return getBlockChildren(client, block_id, {...options, attempts: attemptsLeft})
+        return getBlockChildren(client, block_id, {
+          ...options,
+          attempts: attemptsLeft,
+        });
       }
     }
-    throw err
+    throw err;
   }
-  const fullBlocks: Block[] = []
+  const fullBlocks: Block[] = [];
   for (const block of blocks.results) {
     if (!isFullBlock(block)) {
-      console.log('non full', block)
-      throw new Error('Found non-full block')
+      console.log('non full', block);
+      throw new Error('Found non-full block');
     }
-    let children: Block[] = []
+    let children: Block[] = [];
     if (block.has_children) {
-      children = await getBlockChildren(client, block.id, options)
+      children = await getBlockChildren(client, block.id, options);
     }
-    fullBlocks.push({ block, children })
+    fullBlocks.push({ block, children });
   }
-  return fullBlocks
+  return fullBlocks;
 }
---
+++ src/question.ts
@@ -0,0 +1,136 @@
+import { Client } from '@notionhq/client'
+import {
+  queryDatabaseWithBlocks,
+  APIRequestOptions,
+  API_REQUEST_DEFAULT_OPTIONS,
+  Page,
+  queryDatabase,
+} from './notion'
+import { RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints'
+import type { QueryDatabaseParameters } from '@notionhq/client/build/src/api-endpoints'
+
+export interface Question {
+  id: string
+  question: RichTextItemResponse[]
+  answer: RichTextItemResponse[]
+  questionType: string[]
+  publishable: string | undefined
+}
+
+export interface QuestionType {
+  id: string
+  category: string
+}
+
+const isQuestion = (item: Question | undefined): item is Question => {
+  return Boolean(item)
+}
+
+function parseQuestionPage(
+  page: Page,
+  questionTypesMap: Map<string, string>
+): Question | undefined {
+  const properties = page.page.properties
+
+  const question = properties['Question']
+  if (question?.type != 'title') {
+    throw new Error('Expected Question to be title')
+  }
+
+  const answer = properties['Answer']
+  if (answer?.type != 'rich_text') {
+    throw new Error('Expected Answer to be rich_text')
+  }
+
+  const questionTypeProp = properties['Question type']
+  if (questionTypeProp?.type != 'relation') {
+    throw new Error('Expected Question type to be relation')
+  }
+
+  const publishable = properties['Publishable?']
+  if (publishable?.type != 'select') {
+    throw new Error('Expected Publishable? to be select')
+  }
+
+  const questionTypeIds = questionTypeProp.relation.map((r) => r.id)
+  const questionTypeNames = questionTypeIds
+    .map((id) => questionTypesMap.get(id))
+    .filter((name): name is string => !!name)
+
+  return {
+    id: page.page.id,
+    question: question.title,
+    answer: answer.rich_text,
+    questionType: questionTypeNames,
+    publishable: publishable.select?.name,
+  }
+}
+
+function parseQuestionTypePage(
+  page: any
+): QuestionType | undefined {
+  const properties = page.properties
+  const category = properties['Category']
+  if (category?.type != 'title') {
+    console.warn('Skipping Question Type without Category title')
+    return undefined
+  }
+  
+  // Get plain text title
+  const categoryName = category.title.map((t: any) => t.plain_text).join('')
+
+  return {
+    id: page.id,
+    category: categoryName,
+  }
+}
+
+
+const questionDatabaseId = '2a701a3f59f880db8ebdc93e0dba5ce8'
+const questionTypeDatabaseId = '2a701a3f59f880278472c9c288d64833'
+
+async function getQuestionTypesMap(
+  client: Client,
+  options: APIRequestOptions
+): Promise<Map<string, string>> {
+  // Query the entire Question Type database
+  const pages = await queryDatabase(
+    client,
+    {
+      database_id: questionTypeDatabaseId,
+    },
+    options
+  )
+
+  const map = new Map<string, string>()
+  for (const page of pages) {
+    const qt = parseQuestionTypePage(page)
+    if (qt) {
+      map.set(qt.id, qt.category)
+    }
+  }
+  return map
+}
+
+export async function lookupQuestions(
+  client: Client,
+  query: Omit<QueryDatabaseParameters, 'database_id'>,
+  options: APIRequestOptions = API_REQUEST_DEFAULT_OPTIONS,
+): Promise<Question[]> {
+  // Fetch Questions and Question Types in parallel
+  const [pages, questionTypesMap] = await Promise.all([
+    queryDatabaseWithBlocks(
+      client,
+      {
+        database_id: questionDatabaseId,
+        ...query,
+      },
+      options
+    ),
+    getQuestionTypesMap(client, options),
+  ])
+
+  return pages
+    .map(page => parseQuestionPage(page, questionTypesMap))
+    .filter(isQuestion)
+}
